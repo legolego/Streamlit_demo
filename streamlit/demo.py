12797bc0b854fdd7211c619188ec58109eae3385
Oleg <nikolsky@umich.edu>
2022-10-18 02:42:31 +0000

    renamed csv cols, added plot

--- streamlit/demo.py
+++ streamlit/demo.py
@@ -4,6 +4,7 @@ import streamlit as st
 import os
 from PIL import Image
 from pathlib import Path
+import altair as alt
 
 
 def getPathInStreamlitDir():
@@ -21,9 +22,9 @@ st.image(image)
 
 st.text('This is a dataframe from a csv file.')
 
-df = pd.read_csv(getPathInStreamlitDir() / str('data/IRIS.csv'))
+df_iris = pd.read_csv(getPathInStreamlitDir() / str('data/IRIS.csv'))
 
-st.dataframe(df)
+st.dataframe(df_iris)
 
 st.text('This was the key to be able to get to files with the same line of code in Deepnote and Streamlit')
 
@@ -43,3 +44,15 @@ st.code('''
 /work/Streamlit_demo
 /work
 ''')
+
+st.text("Now let's make an Altair plot")
+
+plot = alt.Chart(df_iris).mark_point().encode(
+  # Map the sepalLength to x-axis
+    x = 'sepalLength',
+  # Map the petalLength to y-axis
+    y = 'petalLength',
+    color = 'variety'
+)
+
+st.altair_chart(plot)
